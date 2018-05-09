61a8f68351661c78feb72967d1193fe22515f7e9
shamanyu <shubhamshamanyu@gmail.com>
2018-05-09 12:57:53 +0530

    Improve goroutines problem of Concurrency section og GolangTour

--- GolangTour/Concurrency/goroutines/goroutines.go
+++ GolangTour/Concurrency/goroutines/goroutines.go
@@ -3,17 +3,18 @@ package main
 import (
   "fmt"
   "time"
+  "strconv"
 )
 
 func say(s string) {
   for i := 0; i < 5; i++{
     time.Sleep(100 * time.Millisecond)
-    fmt.Println(s)
+    fmt.Println(s + " " + strconv.Itoa(i))
   }
 }
 
 func main() {
   go say("world")
   say ("hello")
-  time.Sleep(1000 * time.Millisecond)
+  // time.Sleep(1000 * time.Millisecond)
 }
