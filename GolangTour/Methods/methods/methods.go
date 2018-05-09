6c838d12c79248101442e97448ee30f97b33cf50
shamanyu <shubhamshamanyu@gmail.com>
2018-05-09 19:31:58 +0530

    Extend 'methods' problem of 'Methods' section of 'GolangTour' to support 3D and add a Scaler function

--- GolangTour/Methods/methods/methods.go
+++ GolangTour/Methods/methods/methods.go
@@ -6,14 +6,19 @@ import (
 )
 
 type Vertex struct {
-  X, Y float64
+  X, Y, Z float64
 }
 
 func (v Vertex) Abs() float64 {
-  return math.Sqrt(v.X*v.X + v.Y*v.Y)
+  return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z)
+}
+
+func (v Vertex) Scaler(factor float64) float64 {
+  return v.Abs()*factor
 }
 
 func main() {
-  v := Vertex{3, 4}
+  v := Vertex{3, 4, 10}
   fmt.Println(v.Abs())
+  fmt.Println(v.Scaler(2))
 }
