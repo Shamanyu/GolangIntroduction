a37d8f9dbbf19c61be690dbe4b54452a98f6afc2
shamanyu <shubhamshamanyu@gmail.com>
2018-05-09 19:41:14 +0530

    Extend 'methods-pointers' problem of 'Methods' section of 'GolangTour' to support 3D

--- GolangTour/Methods/methods-pointers/methods-pointers.go
+++ GolangTour/Methods/methods-pointers/methods-pointers.go
@@ -6,20 +6,21 @@ import (
 )
 
 type Vertex struct {
-  X, Y float64
+  X, Y, Z float64
 }
 
 func (v Vertex) Abs() float64 {
-  return math.Sqrt(v.X*v.X + v.Y*v.Y)
+  return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
 }
 
 func (v *Vertex) Scale(f float64) {
   v.X = v.X * f
   v.Y = v.Y * f
+  v.Z = v.Z * f
 }
 
 func main() {
-  v := Vertex{3, 4}
+  v := Vertex{3, 4, 20}
   v.Scale(10)
   fmt.Println(v.Abs())
 }
