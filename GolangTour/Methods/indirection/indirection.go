b8cfb905ae8b10dc49cc442f453e3f70d684921a
shamanyu <shubhamshamanyu@gmail.com>
2018-05-09 20:06:22 +0530

    Enhance 'indirection' program of 'Methods' section of 'GolangTour' to support 3D and try out an additional call

--- GolangTour/Methods/indirection/indirection.go
+++ GolangTour/Methods/indirection/indirection.go
@@ -3,25 +3,28 @@ package main
 import "fmt"
 
 type Vertex struct {
-  X, Y float64
+  X, Y, Z float64
 }
 
 func (v *Vertex) Scale(f float64) {
   v.X = v.X * f
   v.Y = v.Y * f
+  v.Z = v.Z * f
 }
 
 func ScaleFunc(v *Vertex, f float64) {
   v.X = v.X * f
   v.Y = v.Y * f
+  v.Z = v.Z * f
 }
 
 func main() {
-  v := Vertex{3, 4}
+  v := Vertex{3, 4, 5}
+  (&v).Scale(2)
   v.Scale(2)
   ScaleFunc(&v, 10)
 
-  p := &Vertex{4, 3}
+  p := &Vertex{4, 3, 10}
   p.Scale(3)
   ScaleFunc(p, 8)
 
