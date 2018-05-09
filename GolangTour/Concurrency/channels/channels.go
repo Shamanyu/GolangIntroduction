03cb053df685fb6281e596318de3f79b0ab5ba7a
shamanyu <shubhamshamanyu@gmail.com>
2018-05-09 13:38:08 +0530

    Improve 'channels' solution for 'Concurrency' section of 'GolangTour'

--- GolangTour/Concurrency/channels/channels.go
+++ GolangTour/Concurrency/channels/channels.go
@@ -14,9 +14,10 @@ func main() {
   s := []int{7, 2, 8, -9, 4, 0}
 
   c := make(chan int)
-  go sum(s[:len(s)/2], c)
-  go sum(s[len(s)/2:], c)
-  x, y := <-c, <-c
+  go sum(s[:len(s)/3], c)
+  go sum(s[len(s)/3:len(s)*2/3], c)
+  go sum(s[len(s)*2/3:], c)
+  x, y, z := <-c, <-c, <-c
 
-  fmt.Println(x, y, x+y)
+  fmt.Println(x, y, z, x+y+z)
 }
