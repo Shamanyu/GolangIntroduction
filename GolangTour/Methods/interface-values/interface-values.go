5f382660131e60f5d91be4dfaf445675d6896892
shamanyu <shubhamshamanyu@gmail.com>
2018-05-14 14:53:33 +0530

    Extend 'interface-values' problem of 'Methods' section of 'GolangTour'

--- GolangTour/Methods/interface-values/interface-values.go
+++ GolangTour/Methods/interface-values/interface-values.go
@@ -7,14 +7,20 @@ import (
 
 type I interface {
   M()
+  N()
 }
 
 type T struct {
   S string
+  S1 string
 }
 
 func (t *T) M() {
   fmt.Println(t.S)
+}
+
+func (t *T) N() {
+  fmt.Println(t.S1)
 }
 
 type F float64
@@ -23,16 +29,22 @@ func (f F) M() {
   fmt.Println(f)
 }
 
+func (f F) N() {
+  fmt.Println(f)
+}
+
 func main() {
   var i I
 
-  i = &T{"Hello"}
+  i = &T{"Hello", "Bhai"}
   describe(i)
   i.M()
+  i.N()
 
   i = F(math.Pi)
   describe(i)
   i.M()
+  i.N()
 }
 
 func describe(i I) {
