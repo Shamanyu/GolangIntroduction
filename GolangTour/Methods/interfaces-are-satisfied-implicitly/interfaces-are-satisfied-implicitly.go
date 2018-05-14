d44e88eab4ba9d5e4ea22378f249e88f4f40eaeb
shamanyu <shubhamshamanyu@gmail.com>
2018-05-14 14:47:20 +0530

    Enhance interfaces-are-satisfied-implicitly problem of Methods sectiong of GolangTour

--- GolangTour/Methods/interfaces-are-satisfied-implicitly/interfaces-are-satisfied-implicitly.go
+++ GolangTour/Methods/interfaces-are-satisfied-implicitly/interfaces-are-satisfied-implicitly.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type I interface {
   M()
+  N()
 }
 
 type T struct {
@@ -14,7 +15,12 @@ func (t T) M() {
   fmt.Println(t.S)
 }
 
+func (t T) N() {
+  fmt.Println(t)
+}
+
 func main() {
   var i I = T{"hello"}
   i.M()
+  i.N()
 }
