4ffea420fb3d9f76658db4241e942507a9b87732
shamanyu <shubhamshamanyu@gmail.com>
2018-05-20 19:07:23 +0530

    Explore 'structure-new'

---
+++ TheLittleGoBook/structure-new/structure-new.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+type lamePerson struct {
+  name string
+  levelOfLameness int
+}
+
+func main() {
+  doshi := new(lamePerson)
+  flyingParrot := &lamePerson{}
+  fmt.Println(*doshi, *flyingParrot)
+}
