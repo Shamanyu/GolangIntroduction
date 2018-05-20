58d25c20f522edb21a4490c162977fa5644b620a
shamanyu <shubhamshamanyu@gmail.com>
2018-05-20 17:43:57 +0530

    Explore 'structure methods'

---
+++ TheLittleGoBook/structure-methods/structure-methods.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Pokemon struct {
+  name string
+  hp int
+}
+
+func (p *Pokemon) makeEX() {
+  p.hp *= 2
+}
+
+func main() {
+  pikachu := Pokemon{
+    name: "Pikachu",
+    hp: 97,
+  }
+  pikachu.makeEX()
+  fmt.Println(pikachu)
+}
