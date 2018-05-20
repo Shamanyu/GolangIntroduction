c40de9572691fe3713ffbaa9d5732a5185c23a9f
shamanyu <shubhamshamanyu@gmail.com>
2018-05-20 12:04:01 +0530

    Explore 'os' library

---
+++ TheLittleGoBook/os/os.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+  "os"
+)
+
+func main() {
+  fmt.Println("Path of currently running exxecutable is", os.Args[0])
+  if len(os.Args) != 2 {
+    os.Exit(1)
+  }
+  fmt.Println("It's over", os.Args[1])
+}
