a4b931b76664544415ca6bcbc20555a99f83b1c3
shamanyu <shubhamshamanyu@gmail.com>
2018-05-21 11:30:23 +0530

    Explore arrays

---
+++ TheLittleGoBook/array/array.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var scores [10]int
+  scores[0] = 339
+  moreScores := [4]int{9, 1, 4, 10000000}
+  fmt.Println(scores, moreScores)
+  for _, value := range scores {
+    value *= 2
+  }
+  fmt.Println(scores)
+  for index, _ := range scores {
+    scores[index] *= 2
+  }
+  fmt.Println(scores)
+}
