3a8b49e753212adeebdea18b11f5ed1cad7a6914
shamanyu <shubhamshamanyu@gmail.com>
2018-05-21 06:59:05 +0530

    Explore 'structure-composition'

---
+++ TheLittleGoBook/structure-composition/structure-composition.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Pokemon struct {
+  name string
+  hp int
+}
+
+func (p *Pokemon) Introduce() {
+  fmt.Printf("Hi, I am %s\n", p.name)
+}
+
+type AncientPokemon struct {
+  *Pokemon
+  age int
+}
+
+func main() {
+  articuno := &AncientPokemon{
+    Pokemon: &Pokemon{
+      name: "Articuno",
+      hp: 97,
+    },
+    age: 20001,
+  }
+  articuno.Introduce()
+  fmt.Println(*articuno)
+}
