2960acb4a8443e39b24758eac69072396b7f3ba0
shamanyu <shubhamshamanyu@gmail.com>
2018-05-22 15:36:46 +0530

    Explore channels

---
+++ TheLittleGoBook/channels/channels.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+
+type Worker struct {
+  id int
+}
+
+func (w Worker) process(c chan int) {
+  for {
+    data := <-c
+    fmt.Printf("Worker %d got %d\n", w.id, data)
+  }
+}
+
+func main() {
+  c := make(chan int)
+  for i:=0; i<4; i++ {
+    worker := Worker{id: i}
+    go worker.process(c)
+  }
+  for {
+    c <- rand.Int()
+    time.Sleep(time.Millisecond*10)
+  }
+}
