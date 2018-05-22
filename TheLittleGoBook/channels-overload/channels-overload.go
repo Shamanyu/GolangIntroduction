734744bc09806259758560f2268f9c2391f2f686
shamanyu <shubhamshamanyu@gmail.com>
2018-05-22 15:39:36 +0530

    Explore sending data faster than goroutines can process them

---
+++ TheLittleGoBook/channels-overload/channels-overload.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+
+type Worker struct {
+  id int
+}
+
+func (w Worker) process(c chan int) {
+  for {
+    data := <-c
+    fmt.Printf("Worker %d got %d\n", w.id, data)
+    time.Sleep(time.Millisecond*1000)
+  }
+}
+
+func main() {
+  c := make(chan int)
+  for i:=0; i<4; i++ {
+    worker := Worker{id: i}
+    go worker.process(c)
+  }
+  for {
+    c <- rand.Int()
+    time.Sleep(time.Millisecond*10)
+  }
+}
