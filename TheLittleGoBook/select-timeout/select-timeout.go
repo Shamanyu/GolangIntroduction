7c18fde035f3c1d6a0ba46e0787c758ae846662c
shamanyu <shubhamshamanyu@gmail.com>
2018-05-22 16:14:25 +0530

    Explore timeout with select

---
+++ TheLittleGoBook/select-timeout/select-timeout.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+
+type Worker struct {
+  id int
+}
+
+func (w *Worker) process(c chan int) {
+  for {
+    data := <-c
+    fmt.Printf("Worker %d got %d\n", w.id, data)
+    time.Sleep(time.Millisecond*500)
+  }
+}
+
+func main() {
+  c := make(chan int, 2)
+  for i:=0; i<4; i++ {
+    worker := &Worker{id: i}
+    go worker.process(c)
+  }
+  for {
+    select {
+      case c <- rand.Int():
+        fmt.Printf("Length of channel is %d\n", len(c))
+        time.Sleep(time.Millisecond*50)
+      case <- time.After(time.Millisecond*10):
+        fmt.Println("Timed out")
+    }
+    time.Sleep(time.Millisecond*50)
+  }
+}
